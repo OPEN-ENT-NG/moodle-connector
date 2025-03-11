11b3e339e855fbd7a7154f0c9ca8e6c484e551a6
Quentin Périé <perieq@hotmail.fr>
2025-03-11 14:11:02 +0100

    fix(share): [#MODL-294] fix unsharing

--- src/main/java/fr/openent/moodle/core/Field.java
+++ src/main/java/fr/openent/moodle/core/Field.java
@@ -9,5 +9,8 @@ public class Field {
     public static final String ID = "id";
     public static final String COURSES = "courses";
     public static final String ATTEMPTS_NUMBER = "attemptsNumber";
+    public static final String USERS = "users";
+    public static final String UNENROL_USERSUSERS = "unenrol_users";
+    public static final String ROLE = "role";
 
 }
